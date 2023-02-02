1da72426da89a68680175eaae7acf94265b643d4
dimyG <dimgeows@gmail.com>
2023-02-02 17:49:40 +0200

    small changes to ml.py, requirements update

--- ml.py
+++ ml.py
@@ -5,7 +5,8 @@ import os
 from pathlib import Path
 from dotenv import load_dotenv
 import matplotlib.pyplot as plt
-#%%
+from PIL import Image
+from xformers.ops import MemoryEfficientAttentionFlashAttentionOp
 
 src_path = Path('.')
 env_path = src_path / '.env'
@@ -13,45 +14,47 @@ load_dotenv(dotenv_path=env_path)  # load environment variables from .env file i
 huggingface_access_token = os.environ.get("HUGGINGFACE_ACCESS_TOKEN")
 
 
-def img_from_prompt(
-        prompt: str, seed: int = 23, guidance_scale: float = 7.5, num_inference_steps: int = 2000) -> torch.Tensor:
-
+def load_pipeline():
+    # the diffusion model is large (2.7GB) so loading the pipeline is a time-consuming operation
     model1 = "CompVis/stable-diffusion-v1-4"
     model2 = "stabilityai/stable-diffusion-2"
     revision = "fp16"  # loading the weights from the float16 precision branch (instead of float32)
     torch_dtype = torch.float16  # telling diffusers to expect the weights to be in float16 precision
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-
     scheduler = EulerDiscreteScheduler.from_pretrained(model2, subfolder="scheduler")
+    pipe = StableDiffusionPipeline.from_pretrained(model2, revision=revision, scheduler=scheduler,
+                                                   torch_dtype=torch_dtype, use_auth_token=huggingface_access_token)
+    return pipe
 
-    # Load the model
-    pipe = StableDiffusionPipeline.from_pretrained(
-        model2,
-        scheduler=scheduler,
-        revision=revision,
-        torch_dtype=torch_dtype,  # telling diffusers to expect the weights to be in float16 precision
-        use_auth_token=huggingface_access_token
-    )
 
+def img_from_prompt(
+        prompt: str, pipe, seed: int = 1024, height: int = 256, width: int = 256, guidance_scale: float = 7.5,
+        num_inference_steps: int = 10) -> torch.Tensor:
+
+    device = "cuda" if torch.cuda.is_available() else "cpu"
     pipe.to(device)
     if device == "cuda":
-        # gc.collect()
+        # pipe.enable_xformers_memory_efficient_attention(attention_op=MemoryEfficientAttentionFlashAttentionOp)
         torch.cuda.empty_cache()
         pipe.enable_attention_slicing()
 
     # Generate the image
     generator = torch.Generator(device).manual_seed(seed)
-    image = pipe(
-        prompt=prompt, height=256, width=256, guidance_scale=guidance_scale,
-        num_inference_steps=num_inference_steps, generator=generator
-    ).images[0]
+    img: Image = pipe(prompt=prompt, height=height, width=width, guidance_scale=guidance_scale,
+                      num_inference_steps=num_inference_steps, generator=generator).images[0]
 
-    return image
+    return img
 
 
-print(torch.cuda.is_available())
+#%%
+pipe = load_pipeline()
 
-image = img_from_prompt("An astronaut on a horse.")
+#%%
+prompt = "A Japanese old man, wide shot, ultrarealistic uhd faces, Kodak Ultramax 800, pexels, 85mm, casual pose, " \
+         "35mm film roll photo, hard light, detailed skin texture, masterpiece, sharp focus, pretty, perfect, " \
+         "wise, handsome, adorable, Hasselblad, candid street portrait : 6"
+image = img_from_prompt(prompt, pipe)
+# image = Image.open("D:/Projects_D/ΑΣΕΠ τεστ/images/ad1.PNG")
+
 print(type(image))
 # display(image)
 plt.imshow(image)
