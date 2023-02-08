2134d6645d5c8fcec9f71365a3db35cc6e551d1c
dimyG <dimgeows@gmail.com>
2023-02-08 16:31:29 +0200

    in dev env don't load the pipeline in start, to avoid hot reloading slow down

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from dotenv import load_dotenv
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import StreamingResponse
-from ml import img_from_prompt, load_pipeline
-from huggingface_hub import try_to_load_from_cache, scan_cache_dir
+from ml import img_from_prompt, load_pipeline, StableDiffusionPipeline
+# from huggingface_hub import try_to_load_from_cache, scan_cache_dir
 
 src_path = Path('.')
 env_path = src_path / '.env'
@@ -16,7 +16,9 @@ load_dotenv(dotenv_path=env_path)  # load environment variables from .env file i
 # Using the cache_dir parameter could be avoided if we set the HUGGINGFACE_HUB_CACHE environment variable on the shell.
 # But it doesn't work just by adding it in the env file, so we read it from the env file and then set it manually.
 huggingface_hub_cache = os.environ.get("HUGGINGFACE_HUB_CACHE")
+debug = os.environ.get("DEBUG")
 print("huggingface_hub_cache:", huggingface_hub_cache)
+print("debug:", debug)
 
 app = FastAPI()
 
@@ -32,20 +34,29 @@ app.add_middleware(
 
 model1 = "CompVis/stable-diffusion-v1-4"
 model2 = "stabilityai/stable-diffusion-2"
-# print('cache dir:', try_to_load_from_cache(model2, 'snapshots/2880f2ca379f41b0226444936bb7a6766a227587/model_index.json'))
+# try_to_load_from_cache
 # print(scan_cache_dir())
 
-# load the pipeline in memory on host start, and then reuse it for all images you want to generate.
-pipe = load_pipeline(model2, cache_dir=huggingface_hub_cache)
+pipe_initial: StableDiffusionPipeline | None = None
+
+if not debug:
+    # in production: load the pipeline in memory on host start, and then reuse it for all images you want to generate.
+    # in dev: don't load it on start because it slows down the hot reloading on code changes.
+    # Instead, load it on each request to generate_img (which will make it slower, but it's ok for dev)
+    pipe_initial = load_pipeline(model2, cache_dir=huggingface_hub_cache)
 
 
 @app.get("/")
 async def read_root():
-    return {"Hello": "the World"}
+    return {"Hello": " er World"}
 
 
-@app.post("/generate_image/")
-async def generate_img(prompt: str):
+@app.post("/generate_image/", response_model=None)
+async def generate_img(prompt: str, pipe=pipe_initial):
+    # print("generating image with pipeline:", pipe)
+    if not pipe:
+        # in dev we will load the pipe here to avoid slowing app's hot reloading.
+        pipe = load_pipeline(model2, cache_dir=huggingface_hub_cache)
     image = img_from_prompt(prompt, pipe, height=256, width=256, guidance_scale=7.5, num_inference_steps=10)
     memory_stream = io.BytesIO()
     image.save(memory_stream, format='PNG')  # write the image to memory instead of the disk, and return it from there
